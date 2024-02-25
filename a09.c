72ad5f34854fdcfee8a22f0f9127ab924522bad0
Sean Conner <spc@conman.org>
2024-02-24 23:50:52 -0500

    Assert some architectural assumptions made in the code.
    Since C99 (which I'm targetting) doesn't have static asserts, we have to
    make do with that we do have.

--- a09.c
+++ a09.c
@@ -788,6 +788,15 @@ int main(int argc,char *argv[])
     .inbuf     = { .buf = {0}, .widx = 0, .ridx = 0 },
   };
   
+  /*----------------------------------------------------------
+  ; Let's just check some assumptions we're making around here
+  ;-----------------------------------------------------------*/
+  
+  assert(CHAR_BIT          == 8);	            /* byte addressability */
+  assert(sizeof(uint32_t)  == sizeof(float));       /* IEEE-754 float */
+  assert(sizeof(uint64_t)  == sizeof(double));      /* IEEE-754 double */
+  assert((unsigned char)-1 == (unsigned char)0xFF); /* 2s complement */
+  
   format_bin_init(&a09);
   fi = parse_command(argc,argv,&a09);
   
