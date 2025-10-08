f2f7cf017aea61bd8107b7014578df213b99ef9a
Sean Conner <spc@conman.org>
2025-10-08 03:13:42 -0400

    Bug fix---division by future label
    If an expression contains a label that isn't yet defined, but will be, the
    division (or modulus) will fail.  The fix is to ignore the division on pass
    1 and only evaluate the expression on pass 2.

--- expr.c
+++ expr.c
@@ -74,15 +74,25 @@ static bool eval(
     case OP_ADD:  v1->value = v1->value +  v2->value; break;
     case OP_MUL:  v1->value = v1->value *  v2->value; break;
     case OP_DIV:
-         if (v2->value == 0)
-           return message(a09,MSG_ERROR,"E0008: divide by 0 error");
-         v1->value = v1->value / v2->value;
+         if (pass == 1)
+           v1->value = 0;
+         else
+         {
+           if (v2->value == 0)
+             return message(a09,MSG_ERROR,"E0008: divide by 0 error");
+           v1->value = v1->value / v2->value;
+         }
          break;
          
     case OP_MOD:
-         if (v2->value == 0)
-           return message(a09,MSG_ERROR,"E0008: divide by 0 error");
-         v1->value = v1->value % v2->value;
+         if (pass == 1)
+           v1->value = 0;
+         else
+         {
+           if (v2->value == 0)
+             return message(a09,MSG_ERROR,"E0008: divide by 0 error");
+           v1->value = v1->value % v2->value;
+         }
          break;
          
     case OP_EXP:
--- rexpr.c
+++ rexpr.c
@@ -46,7 +46,8 @@ static bool reval(
                   struct fvalue *restrict v1,
                   enum operator           op,
                   struct fvalue *restrict v2,
-                  bool                    fdouble
+                  bool                    fdouble,
+                  int                     pass
 )
 {
   static char const *const toper[] =
@@ -91,15 +92,25 @@ static bool reval(
       case OP_MUL:  v1->value.d = v1->value.d *  v2->value.d; break;
       case OP_EXP:  v1->value.d = pow(v1->value.d,v2->value.d); break;
       case OP_DIV:
-           if (v2->value.d == 0.0)
-             return message(a09,MSG_ERROR,"E0008: divide by 0 error");
-           v1->value.d = v1->value.d / v2->value.d;
+           if (pass == 1)
+             v1->value.d = 0.0;
+           else
+           {
+             if (v2->value.d == 0.0)
+               return message(a09,MSG_ERROR,"E0008: divide by 0 error");
+             v1->value.d = v1->value.d / v2->value.d;
+           }
            break;
            
       case OP_MOD:
-           if (v2->value.d == 0.0)
-             return message(a09,MSG_ERROR,"E0008: divide by 0 error");
-           v1->value.d = fmod(v1->value.d,v2->value.d);
+           if (pass == 1)
+             v1->value.d = 0.0;
+           else
+           {
+             if (v2->value.d == 0.0)
+               return message(a09,MSG_ERROR,"E0008: divide by 0 error");
+             v1->value.d = fmod(v1->value.d,v2->value.d);
+           }
            break;
     }
   }
@@ -126,15 +137,25 @@ static bool reval(
       case OP_MUL:  v1->value.f = v1->value.f *  v2->value.f; break;
       case OP_EXP:  v1->value.f = powf(v1->value.f,v2->value.f); break;
       case OP_DIV:
-           if (v2->value.f == 0.0f)
-             return message(a09,MSG_ERROR,"E0008: divide by 0 error");
-           v1->value.f = v1->value.f / v2->value.f;
+           if (pass == 1)
+             v1->value.f = 0.0f;
+           else
+           {
+             if (v2->value.f == 0.0f)
+               return message(a09,MSG_ERROR,"E0008: divide by 0 error");
+             v1->value.f = v1->value.f / v2->value.f;
+           }
            break;
            
       case OP_MOD:
-           if (v2->value.f == 0.0f)
-             return message(a09,MSG_ERROR,"E0008: divide by 0 error");
-           v1->value.f = fmodf(v1->value.f,v2->value.f);
+           if (pass == 1)
+             v1->value.f = 0.0f;
+           else
+           {
+             if (v2->value.f == 0.0f)
+               return message(a09,MSG_ERROR,"E0008: divide by 0 error");
+             v1->value.f = fmodf(v1->value.f,v2->value.f);
+           }
            break;
     }
   }
@@ -383,7 +404,7 @@ bool rexpr(struct fvalue *pv,struct a09 *a09,struct buffer *buffer,int pass,bool
       {
         if (vsp >= (sizeof(vstack) / sizeof(vstack[0])) - 1)
           return message(a09,MSG_ERROR,"E0065: Internal error---expression parser mismatch");
-        if (!reval(a09,&vstack[vsp + 1],ostack[osp]->op,&vstack[vsp],fdouble))
+        if (!reval(a09,&vstack[vsp + 1],ostack[osp]->op,&vstack[vsp],fdouble,pass))
           return false;
         vsp++;
         osp++;
@@ -406,7 +427,7 @@ bool rexpr(struct fvalue *pv,struct a09 *a09,struct buffer *buffer,int pass,bool
   {
     if (vsp >= (sizeof(vstack) / sizeof(vstack[0])) - 1)
       return message(a09,MSG_ERROR,"E0065: Internal error---expression parser mismatch");
-    if (!reval(a09,&vstack[vsp + 1],ostack[osp]->op,&vstack[vsp],fdouble))
+    if (!reval(a09,&vstack[vsp + 1],ostack[osp]->op,&vstack[vsp],fdouble,pass))
       return false;
     vsp++;
     osp++;
