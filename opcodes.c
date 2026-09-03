8d5b6d34d4e2fda8caaff8de3689c95f21f2339a
Sean Conner <spc@conman.org>
2026-09-03 18:37:30 -0400

    Bug fix---pass a pointer, not the structure with an array

--- opcodes.c
+++ opcodes.c
@@ -1480,7 +1480,7 @@ static bool pseudo_include(struct opcdata *opd)
 
 /**************************************************************************/
 
-static bool incbin(struct opcdata *opd,FILE *fp,long len,long start,struct buffer const filename)
+static bool incbin(struct opcdata *opd,FILE *fp,long len,long start,struct buffer const *filename)
 {
   assert(opd != NULL);
   assert(fp  != NULL);
@@ -1489,33 +1489,33 @@ static bool incbin(struct opcdata *opd,FILE *fp,long len,long start,struct buffe
   long fsize;
   
   if (fseek(fp,0,SEEK_END) == -1)
-    return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename.buf,strerror(errno));
+    return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename->buf,strerror(errno));
   fsize = ftell(fp);
   if (fsize < 0)
-    return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename.buf,strerror(errno));
+    return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename->buf,strerror(errno));
     
   if (fsize == 0)
-    return message(opd->a09,MSG_ERROR,"E0097: %s: contains no data",filename.buf);
+    return message(opd->a09,MSG_ERROR,"E0097: %s: contains no data",filename->buf);
     
   if (start >= fsize)
-    return message(opd->a09,MSG_ERROR,"E0095: %s: not enough data",filename.buf);
+    return message(opd->a09,MSG_ERROR,"E0095: %s: not enough data",filename->buf);
     
   if (len == 0)
     len = fsize - start;
   else if (len > 0)
   {
     if (len > fsize - start)
-      return message(opd->a09,MSG_ERROR,"E0095: %s: not enough data",filename.buf);
+      return message(opd->a09,MSG_ERROR,"E0095: %s: not enough data",filename->buf);
   }
   else
   {
     len = (fsize + len) - start; /* len is < 0, so the + */
     if (len <= 0)
-      return message(opd->a09,MSG_ERROR,"E0095: %s: not enough data",filename.buf);
+      return message(opd->a09,MSG_ERROR,"E0095: %s: not enough data",filename->buf);
   }
   
   if (len > UINT16_MAX - opd->a09->pc)
-    return message(opd->a09,MSG_ERROR,"E0043: %s: file too big",filename.buf);
+    return message(opd->a09,MSG_ERROR,"E0043: %s: file too big",filename->buf);
     
   assert(len   > 0);
   assert(start < fsize);
@@ -1526,14 +1526,14 @@ static bool incbin(struct opcdata *opd,FILE *fp,long len,long start,struct buffe
   opd->truncate = opd->datasz > sizeof(opd->bytes);
   
   if (opd->pass == 1)
-    add_file_dep(opd->a09,filename.buf);
+    add_file_dep(opd->a09,filename->buf);
   else if (opd->pass == 2)
   {
     size_t bsz;
     bool   fill = false;
     
     if (fseek(fp,start,SEEK_SET) == -1)
-      return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename.buf,strerror(errno));
+      return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename->buf,strerror(errno));
       
     do
     {
@@ -1544,7 +1544,7 @@ static bool incbin(struct opcdata *opd,FILE *fp,long len,long start,struct buffe
       bsz           = fread(buffer,1,amount,fp);
       
       if (ferror(fp))
-        return message(opd->a09,MSG_ERROR,"E0044: %s: failed reading",filename.buf);
+        return message(opd->a09,MSG_ERROR,"E0044: %s: failed reading",filename->buf);
         
       assert(amount <= (unsigned)len);
       len -= (long)amount;
@@ -1630,7 +1630,7 @@ static bool pseudo_incbin(struct opcdata *opd)
   if (fp == NULL)
     return message(opd->a09,MSG_ERROR,"E0042: %s: '%s'",filename.buf,strerror(errno));
 
-  rc = incbin(opd,fp,len,start,filename);
+  rc = incbin(opd,fp,len,start,&filename);
   fclose(fp);
   return rc;
 }
